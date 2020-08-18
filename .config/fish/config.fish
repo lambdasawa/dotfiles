23d52aee26bfd8e06edf6cba4e60b12436af3d1c
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-08-18 13:07:51 +0900

    :recycle: Do not override fish function

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,17 +2,52 @@ set -gx EDITOR "code --wait"
 
 set -gx PATH $PATH $HOME/bin
 
-starship --version 2>&1 >/dev/null
-eval (starship init fish)
+function setup-starship
+    if ! command -v starship 2>&1 >/dev/null
+        brew install starship
+    end
 
-direnv --version 2>&1 >/dev/null
-eval (direnv hook fish)
+    eval (starship init fish)
+end
 
-zoxide --version 2>&1 >/dev/null
-zoxide init fish | source
+function setup-direnv
+    if ! command -v direnv 2>&1 >/dev/null
+        brew install direnv
+    end
 
-asdf 2>&1 >/dev/null
-. $HOME/.asdf/asdf.fish
-. $HOME/.asdf/completions/asdf.fish
+    eval (direnv hook fish)
+end
 
-source ~/.config/fish/util.fish
+function setup-zoxide
+    if ! command -v zoxide 2>&1 >/dev/null
+        brew install zoxide
+    end
+
+    zoxide init fish | source
+end
+
+function setup-asdf
+    if [ ! -e ~/.asdf ]
+        brew install coreutils curl git
+        git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+        cd ~/.asdf
+        git checkout (git describe --abbrev=0 --tags)
+    end
+
+    source $HOME/.asdf/asdf.fish
+    source $HOME/.asdf/completions/asdf.fish
+end
+
+function setup-util
+    source ~/.config/fish/util.fish
+end
+
+function setup-all
+    setup-starship
+    setup-direnv
+    setup-zoxide
+    setup-asdf
+    setup-util
+end
+
+setup-all
