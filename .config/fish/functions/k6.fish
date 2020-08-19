bb31c2e5f2d77dcf944960a6c2ef0b661ef74b94
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-08-19 19:09:02 -0400

    :sparkles: Use k6 in debian

--- .config/fish/functions/k6.fish
+++ .config/fish/functions/k6.fish
@@ -1,5 +1,14 @@
 function k6
-    command -v k6 2>&1 >/dev/null || brew install k6
+    if ! has k6
+        if is-mac
+            brew install k6
+        else if is-debian
+            sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+            echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
+            sudo apt update -y
+            sudo apt install -y k6
+        end
+    end
 
     command k6 $argv
 end
