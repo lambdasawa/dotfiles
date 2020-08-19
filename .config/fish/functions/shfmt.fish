826e2018792b26f52951b3e5f29bc5b4606b922b
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-08-19 15:02:07 +0000

    :sparkles: Use shfmt in debian

--- .config/fish/functions/shfmt.fish
+++ .config/fish/functions/shfmt.fish
@@ -1,5 +1,13 @@
 function shfmt
-    command -v shfmt 2>&1 >/dev/null || brew install shfmt
+    if ! has shfmt
+        if is-mac
+            brew install shfmt
+        else if is-debian
+            curl -sSLO https://github.com/mvdan/sh/releases/download/v3.1.2/shfmt_v3.1.2_linux_amd64
+            mv shfmt_v3.1.2_linux_amd64 ~/bin/shfmt
+            chmod u+x ~/bin/shfmt
+        end
+    end
 
     command shfmt $argv
 end
