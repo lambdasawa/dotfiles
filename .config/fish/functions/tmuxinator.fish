453610b388319735f8c6be9da1ad09c3ed5464dd
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-08-19 19:06:13 -0400

    :sparkles: Use tmuxinator in debian

--- .config/fish/functions/tmuxinator.fish
+++ .config/fish/functions/tmuxinator.fish
@@ -1,5 +1,11 @@
 function tmuxinator
-    command -v tmuxinator 2>&1 >/dev/null || brew install tmuxinator
+    if ! has tmuxinator
+        if is-mac
+            brew install tmuxinator
+        else if is-debian
+            sudo apt install -y tmuxinator
+        end
+    end
 
     command tmuxinator $argv
 end
