bd8371ef0629adc47cf0d63f467590fc17342554
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-08-19 19:01:03 -0400

    :sparkles: Use ghq in debian

--- .config/fish/functions/ghq.fish
+++ .config/fish/functions/ghq.fish
@@ -1,5 +1,14 @@
 function ghq
-    command -v ghq 2>&1 >/dev/null || brew install ghq
+    if ! has ghq
+        if is-mac
+            brew install ghq
+        else if is-debian
+            curl -sSLO https://github.com/x-motemen/ghq/releases/download/v1.1.5/ghq_linux_amd64.zip
+            unzip ghq_linux_amd64.zip
+            mv ghq_linux_amd64/ghq ~/bin
+            rm -rf ghq_linux_amd64*
+        end
+    end
 
     command ghq $argv
 end
