0bf91c0d02fdc84771409168c9be43c76f301522
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-08-19 19:09:44 -0400

    :sparkles: Use hexyl in debian

--- .config/fish/functions/hexyl.fish
+++ .config/fish/functions/hexyl.fish
@@ -1,5 +1,11 @@
 function hexyl
-    command -v hexyl 2>&1 >/dev/null || brew install hexyl
+    if ! has hexyl
+        if is-mac
+            brew install hexyl
+        else if is-debian
+            sudo apt install -y hexyl
+        end
+    end
 
     command hexyl $argv
 end
