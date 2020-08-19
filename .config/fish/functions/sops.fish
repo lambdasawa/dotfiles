464f69372127503aff7254bc232ba547209753f2
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-08-19 19:10:04 -0400

    :sparkles: Use sops in debian

--- .config/fish/functions/sops.fish
+++ .config/fish/functions/sops.fish
@@ -1,5 +1,13 @@
 function sops
-    command -v sops 2>&1 >/dev/null || brew install sops
+    if ! has sops
+        if is-mac
+            brew install sops
+        else if is-debian
+            curl -sSLO https://github.com/mozilla/sops/releases/download/v3.6.0/sops-v3.6.0.linux
+            mv sops-v3.6.0.linux ~/bin/sops
+            chmod u+x ~/bin/sops
+        end
+    end
 
     command sops $argv
 end
