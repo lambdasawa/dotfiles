a0e34ff9e588ffc3482fab9a0887a470ed21305c
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-08-19 15:01:35 +0000

    :wrench: Import OpenPGP keyring for asdf-nodejs

--- .config/fish/functions/npm.fish
+++ .config/fish/functions/npm.fish
@@ -1,5 +1,12 @@
 function npm
     if [ ! -e ~/.asdf/shims/npm ]
+        bash -c '
+            export GNUPGHOME="${ASDF_DIR:-$HOME/.asdf}/keyrings/nodejs"
+            mkdir -p "$GNUPGHOME"
+            chmod 0700 "$GNUPGHOME"
+            bash ~/.asdf/plugins/nodejs/bin/import-release-team-keyring
+        '
+
         asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
         asdf install nodejs 14.8.0
     end
