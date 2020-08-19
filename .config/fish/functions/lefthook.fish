019c8b897ccd68ee27d2727acdab1924e4813169
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-08-19 15:00:21 +0000

    :sparkles: Use lefthook in debian

--- .config/fish/functions/lefthook.fish
+++ .config/fish/functions/lefthook.fish
@@ -1,5 +1,14 @@
 function lefthook
-    command -v lefthook 2>&1 >/dev/null || brew install Arkweid/lefthook/lefthook
+    if ! has lefthook
+        if is-mac
+            brew install Arkweid/lefthook/lefthook
+        else if is-debian
+            curl -sSLO https://github.com/Arkweid/lefthook/releases/download/v0.7.2/lefthook_0.7.2_Linux_x86_64.gz
+            gunzip lefthook_0.7.2_Linux_x86_64.gz
+            mv lefthook_0.7.2_Linux_x86_64 ~/bin/lefthook
+            chmod u+x ~/bin/lefthook
+        end
+    end
 
     command lefthook $argv
 end
