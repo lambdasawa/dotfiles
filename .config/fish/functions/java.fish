09b80041c09930a6905593ddd21b6e5e3f52e60e
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-08-24 19:22:31 +0900

    :sparkles: Add java

---
+++ .config/fish/functions/java.fish
@@ -0,0 +1,12 @@
+function java
+    if ! which java | grep .asdf 2>&1 >/dev/null
+        asdf plugin add java
+
+        set v (asdf list all java | grep '^openjdk' | tail -n 1)
+
+        asdf install java $v
+        asdf global java $v
+    end
+
+    command java $argv
+end
