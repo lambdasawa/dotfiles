f892f68b468f1dbeaa2e173c13b641073b103538
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-10-24 05:01:41 +0000

    :sparkles: Use gomi in debian

--- .config/fish/functions/gomi.fish
+++ .config/fish/functions/gomi.fish
@@ -3,8 +3,10 @@ function gomi
         if is-mac
             brew install b4b4r07/tap/gomi
         else if is-debian
-            echo "TODO"
-            exit 1
+            curl -sSLO https://github.com/b4b4r07/gomi/releases/download/v1.1.1/gomi_linux_x86_64.tar.gz
+            tar zxf gomi_linux_x86_64.tar.gz
+            mv gomi ~/bin/
+            rm *.tar.gz
         end
     end
 
