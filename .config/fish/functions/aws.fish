d5be84398d99b00fadb4dea32b1469b66a2eec56
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-10-24 04:57:14 +0000

    :sparkles: Use aws-cli in debian

--- .config/fish/functions/aws.fish
+++ .config/fish/functions/aws.fish
@@ -1,6 +1,10 @@
 function aws
     if ! command -v aws 2>&1 >/dev/null
-        brew install awscli
+        if is-mac
+            brew install awscli
+        else
+            sudo apt install awscli
+        end
     end
 
     command aws $argv
