0810d21cf942ca77d0c8eab3714d566cb085bc48
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-11-03 23:48:06 +0900

    :sparkles: Add gh-repo-create command

--- .config/fish/util.fish
+++ .config/fish/util.fish
@@ -231,6 +231,17 @@ function gh-org-repos -a org
         sed 's/git@github.com://g'
 end
 
+function gh-repo-create
+    gh repo create --private (basename (realpath ..))/(basename $PWD)
+    git-remote-ssh-style
+    set name (basename $PWD)
+    echo "# $name" >README.md
+    git add README.md
+    gitmoji -c
+    git push -u origin master
+    gh repo view -w
+end
+
 function docker-prune
     docker system prune --all --force --volumes
 end
