10f97e0da4f4bda349b25d2ec1df63a9dea63ee7
Tsubasa Irisawa <lambdasawa@gmail.com>
2020-12-15 01:07:36 +0900

    :sparkles: Use cloud_sql_proxy

---
+++ .config/fish/functions/cloud_sql_proxy.fish
@@ -0,0 +1,13 @@
+function cloud_sql_proxy
+    if ! has cloud_sql_proxy
+        if is-mac
+            curl -o ~/bin/cloud_sql_proxy https://dl.google.com/cloudsql/cloud_sql_proxy.darwin.amd64
+        else if is-debian
+            echo "TODO"
+            exit 1
+        end
+        chmod u+x ~/bin/cloud_sql_proxy
+    end
+
+    command cloud_sql_proxy $argv
+end
