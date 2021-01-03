dfb0d5ac5828f50af00988d40aa6b069b0997b1b
Tsubasa Irisawa <lambdasawa@gmail.com>
2021-01-03 12:50:07 +0900

    :sparkles: Install grc when start fish

--- .config/fish/initializers/grc.fish
+++ .config/fish/initializers/grc.fish
@@ -1,3 +1,11 @@
+if ! command -v grc 2>&1 >/dev/null
+    if is-mac
+        brew install grc
+    else if is-debian
+        sudo apt install -y grc
+    end
+end
+
 if [ -e /usr/local/etc/grc.fish ]
     source /usr/local/etc/grc.fish
 end
