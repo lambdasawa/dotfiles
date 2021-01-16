f1e41ba8f32398c4df47c3c4449ea60dca26df88
Tsubasa Irisawa <lambdasawa@gmail.com>
2021-01-16 14:31:13 +0900

    :bug: Fix gcloud-project-select

--- .config/fish/functions/gcloud-project-select.fish
+++ .config/fish/functions/gcloud-project-select.fish
@@ -1,3 +1,7 @@
 function gcloud-project-select
-    gcloud projects list | f | awk '{print $1}'
+    set p (gcloud projects list | f | awk '{print $1}')
+
+    [ -z "$p" ] && exit
+
+    gcloud config set project $p
 end
