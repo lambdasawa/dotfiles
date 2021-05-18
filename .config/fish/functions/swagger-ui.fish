6736de1e824887a837781ff4a14346b53b94b9e9
Tsubasa Irisawa <lambdasawa@gmail.com>
2021-05-18 10:03:30 +0900

    :sparkles: Run open command in swagger-ui command

--- .config/fish/functions/swagger-ui.fish
+++ .config/fish/functions/swagger-ui.fish
@@ -11,11 +11,27 @@ function swagger-ui -a api_file_path port
     set api_file_realpath (realpath $api_file_path)
     set api_file_basename (basename $api_file_realpath)
 
+    set container_name swagger-ui
+
+    docker rm -f $container_name
+
     echo "http://localhost:$port"
     docker run \
+        -itd \
         --rm \
+        --name $container_name \
         --publish $port:8080 \
         --volume $api_file_realpath:/app/$api_file_basename \
         --env SWAGGER_JSON=/app/$api_file_basename \
         swaggerapi/swagger-ui
+
+    set url -sSL http://localhost:$port
+    while ! curl $url 2>&1 >/dev/null
+        echo "Wait server..."
+        sleep 1
+    end
+    echo "Sever open. $url"
+    open $url
+
+    docker logs -f $container_name
 end
