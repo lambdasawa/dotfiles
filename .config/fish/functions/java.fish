9f9e19b7c22dddfe25abf6c700f2305c97794cc9
Tsubasa Irisawa <lambdasawa@gmail.com>
2021-07-19 09:44:02 +0900

    Use openjdk

--- .config/fish/functions/java.fish
+++ .config/fish/functions/java.fish
@@ -2,7 +2,7 @@ function java
     if ! which java | grep .asdf 2>&1 >/dev/null
         asdf plugin add java
 
-        set v (asdf list all java | grep '^openjdk' | tail -n 1)
+        set v (asdf list all java | grep openjdk | grep -v adopt | tail -n 1)
 
         asdf install java $v
         asdf global java $v
