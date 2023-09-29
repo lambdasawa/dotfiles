063c066c0d615e84756bf175de93a3f864d10a60
Tsubasa Irisawa <lambdasawa@gmail.com>
2023-09-29 21:12:26 +0900

    Initial commit

---
+++ config.fish
@@ -0,0 +1,47 @@
+function fish_greeting
+end
+
+function fish_preexec --on-event fish_preexec
+end
+
+function fish_postexec --on-event fish_postexec
+end
+
+function ll
+    if command -v eza >/dev/null 2>&1
+        eza -al $argv
+    else
+        ls -al $argv
+    end
+end
+
+function cd
+    set d "$argv"
+    if [ -z "$argv" ]
+        set d (fd --type d | filter)
+    end
+
+    builtin cd "$d"
+
+    ll
+end
+
+if status is-interactive
+    if [ -e /opt/homebrew/bin/brew ]
+        eval "$(/opt/homebrew/bin/brew shellenv)"
+    end
+
+    if command -v starship >/dev/null 2>&1
+        starship init fish | source
+    end
+
+    if command -v direnv >/dev/null 2>&1
+        direnv hook fish | source
+    end
+
+    if command -v zoxide >/dev/null 2>&1
+        zoxide init fish | source
+    end
+
+    fish_add_path ~/bin
+end
---
+++ setup.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+if [ "$(uname)" == "Darwin" ]; then
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+
+    brew upgrade
+
+    brew install --cask \
+        1password \
+        firefox \
+        visual-studio-code \
+        docker \
+        aws-vault \
+        session-manager-plugin \
+        google-cloud-sdk \
+        intellij-idea \
+        raycast \
+        slack
+
+    brew install \
+        fish \
+        gh \
+        starship \
+        direnv \
+        zoxide \
+        rtx \
+        watchexec \
+        go-task/tap/go-task \
+        1password-cli \
+        eza \
+        bat \
+        difftastic \
+        git-delta \
+        ripgrep \
+        fd \
+        broot \
+        sd \
+        sk \
+        jq \
+        jc \
+        gron \
+        jless \
+        dsq \
+        csview \
+        htmlq \
+        mdcat \
+        pandoc \
+        graphviz \
+        hexyl \
+        ouch \
+        xh \
+        k6 \
+        mitmproxy \
+        mysql \
+        postgresql@14 \
+        sqldef/sqldef/mysqldef \
+        sqldef/sqldef/psqldef \
+        redis \
+        ffmpeg \
+        mpv \
+        sox \
+        awscli \
+        awscurl \
+        scrcpy \
+        yt-dlp
+fi
