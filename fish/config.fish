52ccc3368958a8987c3faed19b85276603654ce4
Tsubasa Irisawa <lambdasawa@gmail.com>
2025-02-07 23:03:29 +0900

    Use vscode-insiders

--- fish/config.fish
+++ fish/config.fish
@@ -139,8 +139,8 @@ if status is-interactive
         set -gx DOTNET_ROOT "/opt/homebrew/opt/dotnet/libexec"
     end
 
-    if which code >/dev/null 2>&1
-        set -gx EDITOR "code --wait"
+    if which code-insiders >/dev/null 2>&1
+        set -gx EDITOR "code-insiders --wait"
     else if which vim >/dev/null 2>&1
         set -gx EDITOR vim
     end
@@ -149,7 +149,7 @@ if status is-interactive
     alias b bat
     alias c 'docker compose'
     alias d docker
-    alias e code
+    alias e code-insiders
     alias f 'find-in-files'
     alias g lazygit
     # alias h ''
@@ -172,8 +172,8 @@ if status is-interactive
     # alias y ''
     # alias z zoxide
 
-    alias ca 'code -a .'
-    alias cr 'code -r .'
+    alias ca 'code-insiders -a .'
+    alias cr 'code-insiders -r .'
 
     alias ua 'cursor -a .'
     alias ur 'cursor -r .'
