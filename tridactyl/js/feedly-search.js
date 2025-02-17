17fd0d031a475a0f754a9676905b45ea62589407
Tsubasa Irisawa <lambdasawa@gmail.com>
2025-02-18 01:24:13 +0900

    Remove tridactyl config

--- tridactyl/js/feedly-search.js
+++
@@ -1,6 +0,0 @@
-window.open(
-  `https://feedly.com/i/discover/sources/search/feed/${encodeURIComponent(
-    location.href
-  )}`,
-  "_blank"
-);
--- tridactyl/js/twitter-intent-open.js
+++
@@ -1,5 +0,0 @@
-window.open(
-  `https://twitter.com/intent/tweet?text=` +
-    encodeURIComponent(`${document.title}\n${location.href}`),
-  "_blank"
-);
